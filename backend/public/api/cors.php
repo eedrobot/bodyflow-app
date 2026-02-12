9f5fcc5e5ceae3f6397a594135a6d778ebcb4b5a
eedrobot <eedrobot@ukr.net>
2026-02-12 11:38:12 +0200

    cors update

--- backend/public/api/cors.php
+++ backend/public/api/cors.php
@@ -18,6 +18,8 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With');
 header('Access-Control-Max-Age: 86400');
 
+header('Content-Type: application/json; charset=utf-8');
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   http_response_code(204);
   exit;
