3fa267a2100de49bebd4b83f053550705b78089f
eedrobot <eedrobot@ukr.net>
2026-02-21 13:01:46 +0200

    social update

--- frontend/components/common/Social.vue
+++ frontend/components/common/Social.vue
@@ -4,7 +4,7 @@
         <a href="support@bodyflow.com.ua">support@bodyflow.com.ua</a>
         </div>
         <div class="social-links">
-            <a class="social-link instagram" href="https://instagram.com/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <a class="social-link instagram" href="https://www.instagram.com/bodyflow_app/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                 <!-- Instagram icon -->
                 <svg class="social-icon" viewBox="0 0 24 24" aria-hidden="true">
                     <path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm9 2h-9A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9A3.5 3.5 0 0 0 20 16.5v-9A3.5 3.5 0 0 0 16.5 4Z"/>
@@ -13,7 +13,7 @@
                 </svg>
             </a>
 
-            <a class="social-link facebook" href="https://facebook.com/" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <a class="social-link facebook" href="https://www.facebook.com/bodyflow.pro" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                 <!-- Facebook icon -->
                 <svg class="social-icon" viewBox="0 0 24 24" aria-hidden="true">
                     <path d="M13.5 22v-8h2.7l.4-3H13.5V9.3c0-.9.2-1.5 1.5-1.5h1.6V5.1c-.3 0-1.2-.1-2.3-.1-2.3 0-3.9 1.4-3.9 4V11H8v3h2.5v8h3Z"/>
